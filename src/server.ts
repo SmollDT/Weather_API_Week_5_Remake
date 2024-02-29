e3636872e73eade24184158a1adfe14bd3140f75
Zohaib <145664940+SmollDT@users.noreply.github.com>
2024-02-29 12:24:56 +0000

    Updated the server file

--- src/server.ts
+++ src/server.ts
@@ -10,5 +10,5 @@ app.use(express.json());
 
 // Start the express server
 app.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}`);
+  console.log(`Server is listening on port ${PORT}`);
 });
