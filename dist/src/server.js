befd82de2cdf4206329a6a42aa2b4fc7fe8c4b03
Zohaib <145664940+SmollDT@users.noreply.github.com>
2024-03-07 12:56:00 +0000

    feat: gitignore update

--- dist/src/server.js
+++ dist/src/server.js
@@ -1,11 +1,13 @@
 import express from "express";
+import weatherRoute from "./routes/weatherRouter.js";
 import cors from "cors";
-// We will create an express app
 const app = express();
 // The port that the express server will listen on
 const PORT = 3000;
 app.use(express.json());
 app.use(cors());
+// Our weather route
+app.use("/api/weather", weatherRoute);
 // Start the express server
 app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
--- src/routes/weatherRouter.ts
+++ src/routes/weatherRouter.ts
@@ -4,6 +4,6 @@ import { validateCityName } from "../middleware/validators.js";
 
 const router = express.Router();
 
-router.get("./:city", validateCityName, getWeatherData);
+router.get("/:city", validateCityName, getWeatherData);
 
 export default router;
